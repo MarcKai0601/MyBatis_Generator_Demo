4388444d8c96e6dd3be9b71017aa73a05cf77497
zheng.kai <zheng.kai@jyintgroup.com>
2024-04-30 18:14:53 +0800

    MyBatis_Generator_Demo

---
+++ src/test/java/com/example/myBatisGenerator/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myBatisGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
